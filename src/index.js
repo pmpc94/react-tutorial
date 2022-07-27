846729b454dd7d43a93cf46237ea24f386f2c96b
pmpc94 <pmig.pc@gmail.com>
2022-07-27 13:42:43 +0100

    declaring the winner

--- src/index.js
+++ src/index.js
@@ -23,6 +23,11 @@ import './index.css';
 
     handleClick(i) {
       const squares = this.state.squares.slice();
+
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+
       squares[i] = this.state.xIsNext ? 'X' : 'O'
       this.setState({ 
         squares: squares, 
@@ -38,7 +43,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
